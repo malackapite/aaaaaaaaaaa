8538b472e95664e673334cb593e36208ba34236a
malackapite <veress.marcell@protonmail.com>
2023-11-06 22:51:55 +0100

    🐱+🧐

--- view/view.js
+++ view/view.js
@@ -6,8 +6,20 @@ class View{
     }
 
     megjelenit(lista, szulElem){
-        szulElem.html("<table class='table table-striped-columns'><thead></thead><tbody></tbody></table>")
-            this.Kiir(lista, szulElem)
+        szulElem.html("<table class='table table-striped'><thead></thead><tbody></tbody></table>")
+        this.fejlec(lista, szulElem)
+        this.Kiir(lista, szulElem)
+    }
+
+    fejlec(lista, szulElem){
+        let tmp=""
+        for (const key in lista[0]) {
+            if (Object.hasOwnProperty.call(lista[0], key)) {
+                tmp+=`<th>${key}</th>`
+            }
+        }
+        szulElem.children("table").eq(0).children("thead").eq(0).append("<tr class='table-dark'>"+tmp+"<th>kijelöl</th><th>mégse</th><th>töröl</th></tr>")
+        
     }
 
     Kiir(lista, szulElem){
